06d824a621fb0fe7aaa4424674826a36d69e7e55
Hyemi Song <ham311@gmail.com>
2019-11-03 11:02:40 -0800

    Updates

---
+++ precache-manifest.2bebb9fa0f94d95f30f3b28c5f0df0dd.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26c83169053a2335df59213d9a1fb81e",
+    "url": "/azsearchUT/index.html"
+  },
+  {
+    "revision": "9a2087e89ab1d2d88536",
+    "url": "/azsearchUT/static/css/main.b98eefe6.chunk.css"
+  },
+  {
+    "revision": "1529fe89e19c582f7a00",
+    "url": "/azsearchUT/static/js/2.17050b5b.chunk.js"
+  },
+  {
+    "revision": "9a2087e89ab1d2d88536",
+    "url": "/azsearchUT/static/js/main.6d41b858.chunk.js"
+  },
+  {
+    "revision": "85db14fb69f45098a48d",
+    "url": "/azsearchUT/static/js/runtime-main.3083c7e6.js"
+  },
+  {
+    "revision": "4d5c2a3ee7787beec4687e2307aa8b66",
+    "url": "/azsearchUT/static/media/Eye_B.4d5c2a3e.svg"
+  },
+  {
+    "revision": "3d4268806ef9ec2556c26be60300a37f",
+    "url": "/azsearchUT/static/media/Info.3d426880.svg"
+  },
+  {
+    "revision": "88333198ec0f2d73b88fdd84d9fe337d",
+    "url": "/azsearchUT/static/media/TrashBtn.88333198.svg"
+  },
+  {
+    "revision": "e1c2dd0539c52bf481bfb0598909d760",
+    "url": "/azsearchUT/static/media/Trash_B.e1c2dd05.svg"
+  },
+  {
+    "revision": "17778ba59223a0816037fa4e723b19fc",
+    "url": "/azsearchUT/static/media/Union59_B.17778ba5.svg"
+  },
+  {
+    "revision": "f857020375ec7fbbbea7827a448ceb48",
+    "url": "/azsearchUT/static/media/addBox_B.f8570203.svg"
+  },
+  {
+    "revision": "adf3b99093f38b8dbb899c6753ea269b",
+    "url": "/azsearchUT/static/media/basic.adf3b990.svg"
+  },
+  {
+    "revision": "b2699a98bc58882dc27916b70c70b5c9",
+    "url": "/azsearchUT/static/media/mainNav.b2699a98.png"
+  },
+  {
+    "revision": "f2355bc52d68ffa17e7cea9efccf26ca",
+    "url": "/azsearchUT/static/media/results.f2355bc5.svg"
+  },
+  {
+    "revision": "ead7b224262429a61962b6a2ba0665d8",
+    "url": "/azsearchUT/static/media/sidebar.ead7b224.svg"
+  },
+  {
+    "revision": "852a4e1fdfdd1b4cbbf6c977154e0408",
+    "url": "/azsearchUT/static/media/suggestions.852a4e1f.svg"
+  },
+  {
+    "revision": "2b1df2d0de2e0064d0cf62f06b793dda",
+    "url": "/azsearchUT/static/media/thumb.2b1df2d0.jpg"
+  },
+  {
+    "revision": "22bc173922bee7b0c77a9313ff41bb54",
+    "url": "/azsearchUT/static/media/topbarRight.22bc1739.png"
+  }
+]);
